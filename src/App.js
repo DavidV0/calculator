7aa5ace721aee7b59cb83fe8fab6bc61479b2598
David Velickovic <davidveli2000@hotmail.com>
2023-04-20 18:10:58 +0200

    hanger

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Calculator from "./components/Calculator";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+        <Calculator></Calculator>
     </div>
   );
 }
